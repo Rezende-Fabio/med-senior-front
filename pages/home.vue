58582428ff6b7738641a89bc92cba6522ea71114
Rezende-Fabio <rezendefabio337@gmail.com>
2023-09-30 17:47:39 -0300

    starting the home page structure

--- pages/home.vue
+++ pages/home.vue
@@ -2,11 +2,23 @@
     <v-navigation-drawer>
         <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
         <v-divider></v-divider>
-        <v-list-item link title="List Item 1"></v-list-item>
-        <v-list-item link title="List Item 2"></v-list-item>
-        <v-list-item link title="List Item 3"></v-list-item>
+        <v-list style="height: 80%;" class="d-flex flex-column justify-space-between" nav>
+            <v-list-item prepend-icon="mdi-pill" title="MEDICAMENTOS" value=""></v-list-item>
+            <v-list-item prepend-icon="mdi-calendar" title="CONSULTAS" value=""></v-list-item>
+            <v-list-item prepend-icon="mdi-file" title="PRONTUÁRIO" value=""></v-list-item>
+        </v-list>
     </v-navigation-drawer>
-    <v-app-bar :elevation="7"></v-app-bar>
+    <v-main>
+        <v-app-bar :elevation="7">
+            <v-btn prepend-icon="mdi-sick">
+                Está com algum desconforto?
+            </v-btn>
+            <v-btn prepend-icon="mdi-phone">
+                Emergência
+            </v-btn>
+        </v-app-bar>
+        <h1>OPA</h1>
+    </v-main>
 </template>
 
 <script setup>
